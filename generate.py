807310b9ebb227d71dcb6a0a016c4d17476c58df
Dylan Strudwick <72671213+n0rmaliz@users.noreply.github.com>
2022-01-31 18:46:15 +0000

    just realised i have to procedurally generate c code ahaha :ojoy:

--- generate.py
+++ generate.py
@@ -1,3 +1,6 @@
 def generate():
+    # this is the part where i do acc kinda the least work pog
+    # but i only know a tiny amount of c lol
+    # we must appease the performance gods
     pass
     return c_src
