29ca68bf1eab420c0566d9413f89a8b0fff27acd
n0rmaliz <72671213+n0rmaliz@users.noreply.github.com>
2022-02-17 12:31:21 +0000

    yeyeyeye

--- parser.py
+++ parser.py
@@ -1,10 +1,18 @@
 from lexer import lexer
 
-def parser(tokens):
-    pass
+def parser(token_gen):
+
+    while True:
+        token_cache = []
+        match = False
+        while not match:
+            current = next(token_gen)
+            token_cache.append(current)
+            match = match_grammar(token_cache)
+
 
 if __name__ == "__main__":
     with open("test programs/big test.weave", "r") as f:
-        tokens = lexer(f.read())
+        token_gen = lexer(f.read())
         parser(tokens)
 
