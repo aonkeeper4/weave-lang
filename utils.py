cbcd3e096e204eadcdf5bdd055ba4b1fec2588d6
unknown <dylanstrudwick1226@gmail.com>
2022-04-30 11:07:09 +0100

    a

--- utils.py
+++ utils.py
@@ -2,3 +2,9 @@ class Token:
     def __init__(self, t, val=""):
         self.type = t
         self.val = val
+
+    def __str__(self):
+        return "Token({}, {})".format(self.type, self.val)
+
+    def __repr__(self):
+        return self.__str__()
