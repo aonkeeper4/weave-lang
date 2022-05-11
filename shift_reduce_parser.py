a98eeb41d1a5716fc6ff1276a7ba453361f544c9
Dylan Strudwick <72671213+n0rmaliz@users.noreply.github.com>
2022-05-11 13:28:20 +0000

    helpe me parejdeojtweinf

--- shift_reduce_parser.py
+++ shift_reduce_parser.py
@@ -8,6 +8,33 @@ class Rule:
         self.rhs = rhs
 
 
+class ASTNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def add(self, top, sub, *, _dir=0):
+        # add a new node to the top in the direction
+        if _dir == 0: # left
+            node = ASTNode(top, self, sub)
+        elif _dir == 1:
+            node = ASTNode(top, sub, self)
+        
+        return node
+
+    def __str__(self, level=0):
+        print('\t' * level + repr(self.value))
+        for child in self.left, self.right:
+            if isinstance(child, ASTNode):
+                child.other_name(level+1)
+            else:
+                print('\t' * (level+1) + str(child))
+
+    def __repr__(self):
+        return self.__str__()
+        
+
 class ShiftReduceParser:
 
     def __init__(self, grammar, input_buffer, stack=None):
@@ -15,6 +42,7 @@ class ShiftReduceParser:
         self.input_buffer = input_buffer
         if stack is None:
             self.stack = list()
+        self.ast = None
 
     def shift(self):
         self.stack += [self.input_buffer[0]]
@@ -31,12 +59,22 @@ class ShiftReduceParser:
             for rule in self.grammar:
                 key = rule.lhs
                 val = rule.rhs
-                if key == self.stack[-len(key):]:
+                prev = self.stack[-len(key):]
+                if key == prev:
                     self.stack = self.stack[:-len(key)] + [val]
+                    if self.ast is None:
+                        
+                        self.ast = ASTNode(*prev)
+                    else:
+                        try:
+                            self.ast.add(prev[1], prev[2], _dir=0)
+                        except IndexError:
+                            self.ast.add(ASTNode(*prev), None, _dir=0)
                     print(f"REDUCE by {key} -> {val}")
                     print(self.stack)
 
         print("ACCEPT")
+        return self.ast
 
 
 if __name__ == "__main__":
@@ -58,4 +96,5 @@ if __name__ == "__main__":
     ]
 
     parser = ShiftReduceParser(shift_reduce_grammar, input_buffer)
-    parser.parse()
+    ast = parser.parse()
+    print(ast)
